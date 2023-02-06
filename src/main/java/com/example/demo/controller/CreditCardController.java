4fc026c3803f422367b9654d922be594c3f520b5
anil.panigrahi@indegene.com <pallabayan007@gmail.com>
2023-02-06 23:21:44 +0000

    updated cors

--- src/main/java/com/example/demo/controller/CreditCardController.java
+++ src/main/java/com/example/demo/controller/CreditCardController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.server.ResponseStatusException;
 
 
@@ -16,7 +17,7 @@ import com.google.gson.Gson;
 import com.example.demo.model.*;
 import com.example.demo.service.CreditCardService;
 
-
+@CrossOrigin("*")
 @RestController 
 @RequestMapping(path = "/api/v1/cc")
 public class CreditCardController {
